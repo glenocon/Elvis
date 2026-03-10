f0e64cbb375a8214004cf2ef9cba923b8d963626
Glen O'Connor <glenoconnor2026@icloud.com>
2026-03-10 14:11:31 +1100

    Initial Commit

---
+++ Elvis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Elvis
+//
+//  Created by Glen O'Connor on 10/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Glen O'Connor on 10/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
